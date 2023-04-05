f0b372075da6156e6e0b5ca0e1e8b514ac0300a5
Veerabadra Lokesh <veerabadralokesh@pitt.edu>
2023-04-04 23:39:39 -0400

    python3

--- chatsystem/chat_server.cpp
+++ chatsystem/chat_server.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
 
     // cout << argv[0] << argv[1] << argv[2] << endl;
     string id = argv[2];
-    string cmd = "python run_chat_server.py -id " + id;
+    string cmd = "python3 run_chat_server.py -id " + id;
     char *command = new char[cmd.length() + 1];
     strcpy(command, cmd.c_str());
     // cout << command << endl;
